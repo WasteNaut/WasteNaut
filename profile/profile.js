fc405e6ee60045dfd1c94cf32a8dc03ab58b9895
brenley-woodard <brenleyew@gmail.com>
2022-12-14 10:32:36 -0800

    added template literals to profile page

--- profile/profile.js
+++ profile/profile.js
@@ -18,16 +18,16 @@ async function fetchAndDisplayProfile() {
     console.log(profile.data, 'profile');
 
     const username = document.createElement('h2');
-    username.textContent = profile.data.username;
+    username.textContent = `${profile.data.username}'s Profile`;
     const imgEl = document.createElement('img');
     imgEl.src = profile.data.avatar;
     const fav_food = document.createElement('p');
-    fav_food.textContent = profile.data.fav_food;
+    fav_food.textContent = `Your favorite food is ${profile.data.fav_food}`;
 
     if (!profile.data.avatar) {
         imgEl.src =
             '../assets/DALL_E_2022-12-09_13.00.00_-_astronaut_coming_out_of_a_galactic_refrigerator_with_produce__modern_logo__bright_design__colorful-removebg-preview.png';
     }
-
+    profileDetailEl.classList.add('profile');
     profileDetailEl.append(username, imgEl, fav_food);
 }
