1c0a4a335af8ee9f82cf5720d2cdfcfb0ee478e3
Ashton Ruiz <ashton.ruiz@yahoo.com>
2022-12-14 17:06:33 -0800

    css

--- profile/profile.js
+++ profile/profile.js
@@ -18,10 +18,13 @@ async function fetchAndDisplayProfile() {
     console.log(profile.data, 'profile');
 
     const username = document.createElement('h2');
-    username.textContent = `${profile.data.username}'s Profile`;
+    username.classList.add('head-user');
+    username.textContent = `${profile.data.username}'s profile`;
     const imgEl = document.createElement('img');
+    imgEl.classList.add('profile-pic');
     imgEl.src = profile.data.avatar;
     const fav_food = document.createElement('p');
+    fav_food.classList.add('food-tag');
     fav_food.textContent = `Your favorite food is ${profile.data.fav_food}`;
 
     if (!profile.data.avatar) {
